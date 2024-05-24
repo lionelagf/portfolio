9dffec28f1af149a0a435697d464ff032c1b3465
Lionel Faure <faure_lionel@hotmail.fr>
2024-05-24 08:38:48 +0200

    Rectification de background attachement pour la version mobile. Ajout du active sur la navbar au scroll.

--- navbar.js
+++ navbar.js
@@ -1,10 +1,25 @@
+// Ajout de la propriété active au clique
+
 const navlinks = document.querySelectorAll('.nav-link')
 
 function activeNav() {
-    navlinks.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
+  navlinks.forEach((l) => l.classList.remove('active'))
+  this.classList.add('active')
 }
 
-navlinks.forEach(l=> l.addEventListener('click', activeNav))
+navlinks.forEach((l) => l.addEventListener('click', activeNav))
 
-activeNav()
+// Ajout de la propriété active au scroll
+function activateSection() {
+  const sections = document.querySelectorAll('section')
+  const navLinks = document.querySelectorAll('.nav-link')
+
+  let index = sections.length
+
+  while (--index && window.scrollY + 0 < sections[index].offsetTop) {}
+
+  navLinks.forEach((link) => link.classList.remove('active'))
+  navLinks[index].classList.add('active')
+}
+
+window.addEventListener('scroll', activateSection)
--- projectCard.js
+++
@@ -1,5 +0,0 @@
-import projects from './projects.json'
-
-function Projects () {
-    
-}
