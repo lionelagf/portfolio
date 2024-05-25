6bf083e537b8b38863e048674a358a6b818b284c
Lionel Faure <faure_lionel@hotmail.fr>
2024-05-25 09:32:02 +0200

    Navbar, class active au scroll

--- navbar.js
+++ navbar.js
@@ -1,25 +1,37 @@
 // Ajout de la propriété active au clique
 
-const navlinks = document.querySelectorAll('.nav-link')
+// const navlinks = document.querySelectorAll('.nav-link')
 
-function activeNav() {
-  navlinks.forEach((l) => l.classList.remove('active'))
-  this.classList.add('active')
-}
+// function activeNav() {
+//   navlinks.forEach((l) => l.classList.remove('active'))
+//   this.classList.add('active')
+// }
 
-navlinks.forEach((l) => l.addEventListener('click', activeNav))
+// navlinks.forEach((l) => l.addEventListener('click', activeNav))
 
 // Ajout de la propriété active au scroll
 function activateSection() {
-  const sections = document.querySelectorAll('section')
+  const sections = document.querySelectorAll('.scroll')
   const navLinks = document.querySelectorAll('.nav-link')
+  const firstSection = document.querySelector('.first-section') // Assurez-vous que cette classe correspond à votre première section
 
   let index = sections.length
 
-  while (--index && window.scrollY + 0 < sections[index].offsetTop) {}
+  // Ajoutez une valeur de décalage de 300 pixels
+  const offset = 300 // Ajustez cette valeur à la hauteur désirée
+
+  while (--index && window.scrollY + offset < sections[index].offsetTop) {}
 
   navLinks.forEach((link) => link.classList.remove('active'))
-  if (navLinks[index]) {
+
+  // Vérifiez si le défilement est au-dessus de la première section 'scroll'
+  if (window.scrollY < sections[0].offsetTop - offset) {
+    // Ajoutez la classe 'active' à la première section si c'est le cas
+    if (firstSection) {
+      navLinks[0].classList.remove('active')
+    }
+  } else if (navLinks[index]) {
+    // Sinon, ajoutez la classe 'active' à l'élément correspondant
     navLinks[index].classList.add('active')
   }
 }
